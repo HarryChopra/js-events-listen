84ac06cf76235e8ed7e22ee2c1e640bf35d8fb87
Harry Chopra <harrychopra@gmail.com>
2022-02-16 19:22:40 +0000

    Done

---
+++ README.md
@@ -0,0 +1,6 @@
+# JavaScript Event Listeners
+
+## Task
+
+Add an event listener for the `click` event
+on the `input#input` element in `index.html`.
---
+++ index.js
@@ -0,0 +1,6 @@
+function addingEventListener() {
+    const input = document.querySelector('input#input');
+    input.addEventListener('click', () => {
+        alert('I was clicked');
+    });
+}
